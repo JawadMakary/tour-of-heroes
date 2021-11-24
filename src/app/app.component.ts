a3ce53e94d64a978a138a424dd0b4abd798c1245
jawad <jawadhalbymakary@gmail.com>
2021-11-24 18:19:46 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'aline',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+
+  title = 'Tour of Heroes';
+
+
+
+  constructor(){
+   
+  }
+}
