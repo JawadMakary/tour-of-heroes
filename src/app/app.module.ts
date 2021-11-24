bde513cb2e23dbad15b3102e3a210ccf2ac9007f
jawad <jawadhalbymakary@gmail.com>
2021-11-24 23:52:24 +0200

    step 2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-   
-  ],
+  declarations: [AppComponent, HeroesComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
     // Although ngModel is a valid Angular directive, it isn't available by default.
     // It belongs to the optional FormsModule and you must opt-in to using it.
-
-
-
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-
- }
+export class AppModule {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-@Component({ // decorator function that specifies the Angular metadata for the component.
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  // decorator function that specifies the Angular metadata for the component.
   // selector— the component's CSS element selector
-// templateUrl— the location of the component's template file.
- // styleUrls— the location of the component's private CSS styles.
+  // templateUrl— the location of the component's template file.
+  // styleUrls— the location of the component's private CSS styles.
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'the progress click'
-  };
+  heroes = HEROES;
 
-  constructor() { }
+  // create a property to hold the selected hero so we can use it in the template
+  selectedHero?: Hero;
 
-  ngOnInit(): void {
+  // method to handle the event emitted by the child component
+  // when the child component calls the parent component's
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+  // on select : set the selectedHero property to the hero that was clicked
+  constructor() {}
 
+  ngOnInit(): void {}
 }
